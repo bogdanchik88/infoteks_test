4b4d4a7610dba6a770bc62d5d33e8da702ffd4c2
Bogdanchik88 <Bogdanchebotarev@gmail.com>
2026-02-26 11:34:39 +0300

    setup and testing tailwind CSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+
 
 function App() {
   const [count, setCount] = useState(0)
@@ -13,7 +13,7 @@ function App() {
   }
 
   return (
-    <>
+    <div className='flex flex-col gap-6 justify-center w-screen h-screen items-center'>
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -23,18 +23,13 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => api()}>
+        <button className='border-2 border-black bg-amber-100 hover:bg-amber-700 active:bg-violet-800' onClick={() => api()}>
           спарсить юзеров
         </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          Tailwind eto kruto
         </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
