6c0534b9fb01a3487ccae72df00f451184ef3f3c
Bogdanchik88 <Bogdanchebotarev@gmail.com>
2026-03-02 06:23:17 +0300

    fixing table skeleton

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -11,10 +11,10 @@ const Table = () => {
     } = useUsers()
 
   return (
-      <div className="relative overflow-auto max-w-350 flex justify-center w-full">
+      <div className="relative overflow-auto max-w-350 flex justify-center w-full min-h-100">
         {(isLoading || isRefetching) && <Loading/>}
-        {users.length > 0 && isLoading && <TableSkeleton/>}
-        {<table className="border-collapse border border-gray-400 w-full">
+        {isLoading && <TableSkeleton/>}
+        {users.length > 0 && <table className="border-collapse border border-gray-400 w-full">
           <thead>
             <tr>
               <th className="border p-2 cursor-pointer">First Name</th>
--- src/components/TableSkeleton.jsx
+++ src/components/TableSkeleton.jsx
@@ -1,23 +1,39 @@
-const TableSkeleton = ({ rows = 8, columns = 8 }) => {
+const TableSkeleton = ({ rows = 8 }) => {
+
   return (
-    <table className="border-collapse border border-gray-400 w-full">
-      <thead>
-        <tr>
-          {Array.from({ length: columns }).map((_, i) => (
-            <th key={i} className="border p-2 bg-gray-200">&nbsp;</th>
+    <div className="max-w-350 overflow-x-visible flex justify-center w-full min-h-100">
+      <table className="border-collapse border border-gray-400 w-full">
+          <thead>
+            <tr>
+              <th className="border p-2 cursor-pointer">First Name</th>
+              <th className="border p-2 cursor-pointer">Last Name</th>
+              <th className="border p-2 cursor-pointer">Maiden Name</th>
+              <th className="border p-2 cursor-pointer">Age</th>
+              <th className="border p-2 cursor-pointer">Gender</th>
+              <th className="border p-2 cursor-pointer">Phone</th>
+              <th className="border p-2 cursor-pointer">Email</th>
+              <th className="border p-2 cursor-pointer">Country</th>
+              <th className="border p-2 cursor-pointer">City</th>
+            </tr>
+          </thead>
+        <tbody>
+          {fakeUsers.map((user, r) => (
+            <tr key={r}>
+              <td className="border p-2 bg-gray-100">fakeFirstName</td>
+              <td className="border p-2 bg-gray-100">fakeSecondName</td>
+              <td className="border p-2 bg-gray-100">fakeMaidenName</td>
+              <td className="border p-2 bg-gray-100">fakeAge</td>
+              <td className="border p-2 bg-gray-100">fakeGender</td>
+              <td className="border p-2 bg-gray-100">fakePhone</td>
+              <td className="border p-2 bg-gray-100">fakeEmail</td>
+              <td className="border p-2 bg-gray-100">fakeCountry</td>
+              <td className="border p-2 bg-gray-100">fakeCity</td>
+            </tr>
           ))}
-        </tr>
-      </thead>
-      <tbody>
-        {Array.from({ length: rows }).map((_, r) => (
-          <tr key={r} className="animate-pulse">
-            {Array.from({ length: columns }).map((_, c) => (
-              <td key={c} className="border p-2 bg-gray-100">&nbsp;</td>
-            ))}
-          </tr>
-        ))}
-      </tbody>
-    </table>
+        </tbody>
+      </table>      
+    </div>
+
   )
 }
 
